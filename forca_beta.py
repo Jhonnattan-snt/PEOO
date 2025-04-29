f9a1d00ce2f5900cea2f81f9ce96101b08602aad
Jhonnattan <jhonnattansantos2004@gmail.com>
2025-04-29 09:17:52 -0300

    Jogo da Forca (Versão Beta)

---
+++ forca_beta.py
@@ -0,0 +1,22 @@
+import random
+
+palavras = ['POLICIAL', 'MORANGO', 'PROGRAMADOR', 'GOIABA', 'CANTOR']
+palavra = random.choice(palavras)
+descobertas = ['_'] * len(palavra)
+erros = 0
+
+while erros < 6 and '_' in descobertas:
+    print(' '.join(descobertas))
+    letra = input('Palavra: ').upper()
+    if letra in palavra:
+        for i in range(len(palavra)):
+            if palavra[i] == letra:
+                descobertas[i] = letra
+    else:
+        erros += 1
+
+print(' '.join(descobertas))
+if '_' not in descobertas:
+    print('Parabéns você ganhou')
+else:
+    print('Você perdeu, a palavra correta era {palavra}')
